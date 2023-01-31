293bc694207e2a028f93fcf9aabee8f4b8ccc160
Feliks Mukhtarov <mukhtarovfeliks@gmail.com>
2023-01-31 15:22:53 +0300

    Initial commit

---
+++ VSCO/Editor/EditorViewController.swift
@@ -0,0 +1,53 @@
+//
+//  EditorViewController.swift
+//  VSCO
+//
+//  Created by Feliks Mukhtarov on 27.01.2023.
+//
+
+import UIKit
+
+class EditorViewController: UIViewController {
+
+    
+    public var picture: UIImage?
+    
+    @IBOutlet var filterView: UIView!
+    @IBOutlet var imageView: UIImageView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.imageView.image = picture
+        filterView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
+    }
+    
+    @IBAction func action1(_ sender: Any) {
+        filterView.backgroundColor = UIColor.Filter.yellow
+    }
+    
+    @IBAction func action2(_ sender: Any) {
+        filterView.backgroundColor = UIColor.Filter.blue
+
+    }
+    
+    @IBAction func action3(_ sender: Any) {
+        filterView.backgroundColor = UIColor.Filter.grey
+
+    }
+    
+    @IBAction func action4(_ sender: Any) {
+        filterView.backgroundColor = UIColor.Filter.orange
+
+    }
+    
+    @IBAction func action5(_ sender: Any) {
+        filterView.backgroundColor = UIColor.Filter.purple
+
+    }
+    
+    
+    
+    
+}
---
+++ VSCO/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  VSCO
+//
+//  Created by Feliks Mukhtarov on 27.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate {
+
+    @IBOutlet var addButton: UIButton!
+    @IBOutlet var picture: UIImageView!
+    
+    
+    private var imagePicker: ImagePicker!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
+    }
+    
+    @IBAction func editAction(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Editor", bundle: nil)
+        let vc = storyboard.instantiateViewController(identifier: "Editor") as! EditorViewController
+        vc.picture = self.picture.image
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func addAction(_ sender: Any) {
+        self.imagePicker.present(from: sender as! UIView)
+        
+    }
+}
+
+
+
+
+
+
+extension ViewController: ImagePickerDelegate {
+    func didSelect(image: UIImage?) {
+        self.picture.image = image
+    }
+    
+    
+}
